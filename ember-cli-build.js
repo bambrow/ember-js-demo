ae40ce302e725f44ff396063194faaa4f99074e7
bambrow <bambrow@icloud.com>
2020-08-19 19:38:47 +0000

    disable fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
     fingerprint: {
-      exclude: ['/assets/images/'],
+      enabled: false,
     }
   });
 
